dd64c45414c342ab4e98d1603e2c06da36d9ee8d
Caznix <caznix01@gmail.com>
2025-01-16 14:49:31 -0500

    vscode extensions

--- vscode.nix
+++ vscode.nix
@@ -7,6 +7,21 @@ programs.vscode = {
     mutableExtensionsDir = false;
     extensions = (with pkgs.vscode-extensions; [
             rust-lang.rust-analyzer
+            svelte.svelte-vscode
+            bradlc.vscode-tailwindcss
+            ms-vsliveshare.vsliveshare
+            ms-vscode.cmake-tools
+            ms-python.python
+            bierner.markdown-preview-github-styles
+            bierner.markdown-checkbox
+            bierner.markdown-emoji
+            bierner.markdown-footnotes
+            bierner.markdown-mermaid
+            denoland.vscode-deno
+            geequlim.godot-tools
+            gruntfuggly.todo-tree
+            mhutchie.git-graph
+            fill-labs.dependi
             (pkgs.vscode-utils.buildVscodeMarketplaceExtension {
                 mktplcRef = {
                   name = "darcula-solid";
@@ -15,7 +30,6 @@ programs.vscode = {
                   hash = "sha256-tIfCkOR1Z/uRWiZhrBfOQCZT3Cu6yNjAnxjn0UJFO2U=";
                 };
             })
-
         ]);
 
     userSettings = {
